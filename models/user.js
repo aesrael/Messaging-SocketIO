c71b59017a9e0757343a11a6bfa8f44659b000e7
Israel Adura <israeladura@gmail.com>
2017-11-25 00:26:51 +0100

    commit

--- models/user.js
+++ models/user.js
@@ -12,21 +12,50 @@ var UserSchema = mongoose.Schema({
 
 var User = module.exports = mongoose.model('User', UserSchema);
 
+//hash password
 module.exports.createUser = function (newUser, callback) {
-    newUser.save(newUser, callback);
+    bcrypt
+        .genSalt(10, function (err, salt) {
+            bcrypt
+                .hash(newUser.password, salt, function (err, hash) {
+                    // Store hash in your password DB.
+                    newUser.password = hash;
+                    newUser.save(callback);
+                });
+        });
 }
-module.exports.findUserByUsername = function (username, callback) {
+//custom functions for accessing the database
+module.exports.getUserByUsername = function (username, callback) {
     var query = {
         username: username
     };
     User.findOne(query, callback);
 }
-module.exports.findUserById = function (id, callback) {
+module.exports.comparePassword = function (password, hash, callback) {
+    bcrypt
+        .compare(password, hash, function (err, isMatch) {
+            if (err) 
+                throw err;
+            callback(null, isMatch);
+        });
+}
+module.exports.getUserById = function (id, callback) {
     User.findById(id, callback);
+}
+module.exports.getAllUsers = function (callback) {
+    User.find({}, callback);
+}
+module.exports.comparePassword = function (candidatePassword, hash, callback) {
+    bcrypt
+        .compare(candidatePassword, hash, function (err, isMatch) {
+            if (err) 
+                throw err;
+            callback(null, isMatch);
+        });
 }
 module.exports.updateUser = function (id, source, callback) {
     User.findByIdAndUpdate(id, source, callback);
 }
 module.exports.deleteUser = function (id, callback) {
-    User.findByIdAndRemove(id, callback);
+    User.findByIdAndRemove(id, callback)
 }
